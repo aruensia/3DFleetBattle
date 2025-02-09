39da77aa00b7c9a02c3eb26f404a2aa7b591e40c
aruensia <aruensia@naver.com>
2025-02-09 23:39:58 +0900

    [Add] 상점 스크립트 추가.
    ShopMain 스크립트 생성만 하였고, 주석을 통해서 어떤 걸 만들어야 할 지 작성함.

---
+++ Assets/Resources/Script/Shop/ShopMain.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopMain : MonoBehaviour
+{
+    //상점은 유저 전투 등급에 따라 아이쇼핑할 수 있는 상품 개수가 증가한다.
+    //한 번의 전투가 끝날 때마다 아이템의 목록을 새로고침한다. 유저가 특정 상품을 구매할 경우, 솔드아웃이 되어, 더이상 목록에 상품이 생성되지 않는다.
+    //
+
+    private void Start()
+    {
+        // TODO:각 메뉴에 상점 데이터를 불러오는 함수();
+    }
+
+
+    //각 메뉴에 상점 데이터를 불러오는 함수();
+    //{
+
+
+
+
+
+
+
+    //}
+
+
+}
